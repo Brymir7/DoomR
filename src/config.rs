624aa986933ae6ac271562696e1843cbfb10e01a
Brymir7 <Brymir7@gmail.com>
2024-08-25 01:52:13 +0200

    working flooor (and ceiling is same logic almost), but very slow due to tons of draw calls

--- src/config.rs
+++ src/config.rs
@@ -1,20 +1,42 @@
 pub mod config {
     use std::f32::consts::PI;
     pub const WORLD_LAYOUT: [[u8; 15]; 9] = [
-        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-        [1,2,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,1,0,0,0,0,0,1,1,1,1,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,1,0,0,1],
-        [1,0,0,0,0,1,0,0,0,0,0,0,0,0,1],
-        [1,0,1,0,0,1,0,0,0,0,1,0,1,0,1],
-        [1,0,0,0,0,1,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,1,0,0,1],
-        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+        [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1],
+        [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    ];
+    pub const FLOOR_LAYOUT: [[u8; 15]; 9] = [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    ];
+    pub const ROOF_LAYOUT: [[u8; 15]; 9] = [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     ];
     pub const SCREEN_WIDTH: usize = 1920;
-    pub const HALF_SCREEN_WIDTH: f32 = SCREEN_WIDTH as f32 / 2.0;
+    pub const HALF_SCREEN_WIDTH: f32 = (SCREEN_WIDTH as f32) / 2.0;
     pub const SCREEN_HEIGHT: usize = 1080;
-    pub const HALF_SCREEN_HEIGHT: f32 = SCREEN_HEIGHT as f32 / 2.0;
+    pub const HALF_SCREEN_HEIGHT: f32 = (SCREEN_HEIGHT as f32) / 2.0;
     pub const WORLD_WIDTH: usize = WORLD_LAYOUT[0].len() as usize;
     pub const WORLD_HEIGHT: usize = WORLD_LAYOUT.len() as usize;
     pub const PHYSICS_FRAME_TIME: f32 = 1.0 / 60.0;
@@ -22,6 +44,6 @@ pub mod config {
     pub const TILE_SIZE_Y_PIXEL: usize = SCREEN_HEIGHT / WORLD_HEIGHT;
     pub const PLAYER_FOV: f32 = PI / 2.0;
     pub const MAX_VIEW_DISTANCE: usize = WORLD_WIDTH;
-    pub const MAP_X_OFFSET: f32 = SCREEN_WIDTH as f32 * 0.75;
-    pub const MAP_Y_OFFSET: f32 =  SCREEN_HEIGHT as f32 * 0.25;
+    pub const MAP_X_OFFSET: f32 = (SCREEN_WIDTH as f32) * 0.75;
+    pub const MAP_Y_OFFSET: f32 = (SCREEN_HEIGHT as f32) * 0.25;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,45 @@
 use core::panic;
-use std::{ cmp::Ordering, f32::consts::PI, thread::sleep, time::Duration };
-use config::config::{
-    HALF_SCREEN_HEIGHT, HALF_SCREEN_WIDTH, MAP_X_OFFSET, PHYSICS_FRAME_TIME, SCREEN_HEIGHT, SCREEN_WIDTH, TILE_SIZE_Y_PIXEL, WORLD_HEIGHT, WORLD_WIDTH
+use std::{
+    cmp::Ordering,
+    collections::HashMap,
+    f32::consts::PI,
+    ops::Range,
+    thread::sleep,
+    time::Duration,
 };
-use macroquad::prelude::*;
+use config::config::{
+    HALF_SCREEN_HEIGHT,
+    HALF_SCREEN_WIDTH,
+    MAP_X_OFFSET,
+    MAP_Y_OFFSET,
+    PHYSICS_FRAME_TIME,
+    PLAYER_FOV,
+    SCREEN_HEIGHT,
+    SCREEN_WIDTH,
+    TILE_SIZE_X_PIXEL,
+    TILE_SIZE_Y_PIXEL,
+    WORLD_HEIGHT,
+    WORLD_WIDTH,
+};
+use once_cell::sync::Lazy;
+use macroquad::{ color, prelude::*, text };
 pub mod config;
+#[derive(Hash, Eq, PartialEq, Copy, Clone)]
+enum Textures {
+    Stone,
+}
 
+static TEXTURE_TYPE_TO_TEXTURE2D: Lazy<HashMap<Textures, Texture2D>> = Lazy::new(|| {
+    let mut map = HashMap::new();
+    map.insert(
+        Textures::Stone,
+        Texture2D::from_file_with_format(
+            include_bytes!("textures/stone.png"),
+            Some(ImageFormat::Png)
+        )
+    );
+    map
+});
 fn window_conf() -> Conf {
     Conf {
         window_title: "DoomR".to_owned(),
@@ -84,8 +118,10 @@ impl RaycastSystem {
     ) -> Vec<RaycastResult> {
         let mut results = Vec::new();
         for i in 0..SCREEN_WIDTH {
-            let ray_angle = player_angle + config::config::PLAYER_FOV / 2.0 -
-                (i as f32 / SCREEN_WIDTH as f32) * config::config::PLAYER_FOV;
+            let ray_angle =
+                player_angle +
+                config::config::PLAYER_FOV / 2.0 -
+                ((i as f32) / (SCREEN_WIDTH as f32)) * config::config::PLAYER_FOV;
 
             if let Some(result) = RaycastSystem::daa_raycast(origin, ray_angle, tile_map) {
                 results.push(result);
@@ -107,14 +143,14 @@ impl RaycastSystem {
         let mut curr_map_tile_x = origin.x.trunc() as usize;
         let mut curr_map_tile_y = origin.y.trunc() as usize;
         let mut dist_side_x = if direction.x < 0.0 {
-            (origin.x - curr_map_tile_x as f32) * relative_tile_dist_x
+            (origin.x - (curr_map_tile_x as f32)) * relative_tile_dist_x
         } else {
-            (curr_map_tile_x as f32 + 1.0 - origin.x) * relative_tile_dist_x
+            ((curr_map_tile_x as f32) + 1.0 - origin.x) * relative_tile_dist_x
         };
         let mut dist_side_y = if direction.y < 0.0 {
-            (origin.y - curr_map_tile_y as f32) * relative_tile_dist_y
+            (origin.y - (curr_map_tile_y as f32)) * relative_tile_dist_y
         } else {
-            (curr_map_tile_y as f32 + 1.0 - origin.y) * relative_tile_dist_y
+            ((curr_map_tile_y as f32) + 1.0 - origin.y) * relative_tile_dist_y
         };
         while
             curr_map_tile_x < WORLD_WIDTH && // assume it hits a wall before reaching the end of the map
@@ -192,31 +228,97 @@ impl RenderMap {
                 1.0,
                 WHITE
             );
-            draw_circle(
-                result.intersection_pos.x * (config::config::TILE_SIZE_X_PIXEL as f32) * 0.25 +
-                    MAP_X_OFFSET,
-                result.intersection_pos.y * (config::config::TILE_SIZE_Y_PIXEL as f32) * 0.25,
-                2.0,
-                WHITE,
-            );
+            // draw_circle(
+            //     result.intersection_pos.x * (config::config::TILE_SIZE_X_PIXEL as f32) * 0.25 +
+            //         MAP_X_OFFSET,
+            //     result.intersection_pos.y * (config::config::TILE_SIZE_Y_PIXEL as f32) * 0.25,
+            //     2.0,
+            //     WHITE
+            // );
         }
     }
 }
 struct RenderPlayerPOV;
 impl RenderPlayerPOV {
-    fn render(raycast_result: &Vec<RaycastResult>) {
+    fn render_floor(
+        player_angle: f32,
+        player_pos: Vec2,
+        floor_layout: &[[Textures; WORLD_WIDTH]; WORLD_HEIGHT],
+    ) {
+        
+        const HALF_PLAYER_FOV: f32 = PLAYER_FOV / 2.0;
+        let left_most_ray_dir = Vec2::new(
+            (player_angle - HALF_PLAYER_FOV).cos(),
+            (player_angle - HALF_PLAYER_FOV).sin(),
+        );
+        let right_most_ray_dir = Vec2::new(
+            (player_angle + HALF_PLAYER_FOV).cos(),
+            (player_angle + HALF_PLAYER_FOV).sin(),
+        );
+    
+        let floor_step = (right_most_ray_dir - left_most_ray_dir) / SCREEN_WIDTH as f32;
+        for row in HALF_SCREEN_HEIGHT as usize..SCREEN_HEIGHT {
+            let row_distance = HALF_SCREEN_HEIGHT / (row as f32 - HALF_SCREEN_HEIGHT).max(0.01); // near  screen middle the distance approaches infinity 
+            let floor_ray = left_most_ray_dir * row_distance;
+            let mut floor_pos = player_pos + floor_ray;
+            for col in 0..SCREEN_WIDTH {
+                let floor_tile_pos = floor_pos.as_uvec2();
+    
+                if let Some(&texture_type) = floor_layout
+                    .get(floor_tile_pos.y as usize)
+                    .and_then(|row| row.get(floor_tile_pos.x as usize))
+                {
+                    if let Some(texture) = TEXTURE_TYPE_TO_TEXTURE2D.get(&texture_type) {
+                        let texture_coords = Vec2::new(
+                            floor_pos.x.fract() * texture.width() as f32,
+                            floor_pos.y.fract() * texture.height() as f32,
+                        );
+    
+                        // Apply distance-based shading
+                        let shade = (1.0 - (row_distance / 100.0)).clamp(0.0, 1.0);
+                        let color = Color::new(shade, shade, shade, 1.0);
+    
+                        draw_texture_ex(
+                            texture,
+                            col as f32,
+                            row as f32,
+                            color,
+                            DrawTextureParams {
+                                source: Some(Rect::new(
+                                    texture_coords.x,
+                                    texture_coords.y,
+                                    1.0,
+                                    1.0,
+                                )),
+                                ..Default::default()
+                            },
+                        );
+                    }
+                }
+                floor_pos += floor_step * row_distance;
+            }
+        }
+    }
+    
+    fn render_world(raycast_result: &Vec<RaycastResult>) {
         for (i, result) in raycast_result.iter().enumerate() {
-            let  wall_height = (SCREEN_HEIGHT as f32 / (result.distance - 0.5 + 0.000001)  ).min(
-                    SCREEN_HEIGHT as f32 * 0.8
-                );
+            let wall_height = ((SCREEN_HEIGHT as f32) / (result.distance - 0.5 + 0.000001)).min(
+                SCREEN_HEIGHT as f32
+            );
             let wall_color = match result.entity {
                 EntityType::Wall => GREEN,
                 _ => WHITE,
             };
-            let shade = 1.0 - (result.distance / WORLD_WIDTH.max(WORLD_HEIGHT) as f32).clamp(0.0, 1.0);
-            let wall_color = Color::new(wall_color.r * shade, wall_color.g * shade, wall_color.b * shade, 1.0);
+            let shade =
+                1.0 - (result.distance / (WORLD_WIDTH.max(WORLD_HEIGHT) as f32)).clamp(0.0, 1.0);
+            let wall_color = Color::new(
+                wall_color.r * shade,
+                wall_color.g * shade,
+                wall_color.b * shade,
+                1.0
+            );
             let wall_color = if result.hit_from_x_side {
-                wall_color 
+                wall_color
             } else {
                 Color::new(wall_color.r * 0.8, wall_color.g * 0.8, wall_color.b * 0.8, 1.0)
             };
@@ -238,6 +340,8 @@ struct RaycastResult {
 }
 struct World {
     world_layout: [[EntityType; WORLD_WIDTH]; WORLD_HEIGHT],
+    floor_layout: [[Textures; WORLD_WIDTH]; WORLD_HEIGHT],
+    roof_layout: [[Textures; WORLD_WIDTH]; WORLD_HEIGHT],
     walls: Vec<Vec2>,
     player: Player,
 }
@@ -270,8 +374,12 @@ impl World {
                 }
             }
         }
+        let floor_layout = [[Textures::Stone; WORLD_WIDTH]; WORLD_HEIGHT];
+        let roof_layout = [[Textures::Stone; WORLD_WIDTH]; WORLD_HEIGHT];
         Self {
             world_layout,
+            floor_layout,
+            roof_layout,
             walls,
             player,
         }
@@ -297,6 +405,7 @@ impl World {
         WallCollisionSystem::update_player(&mut self.player.pos, &self.walls);
     }
     fn draw(&self) {
+        clear_background(LIGHTGRAY);
         let player_ray_origin = self.player.pos + Vec2::new(0.5, 0.5);
         let start_time = get_time();
         let raycast_results = RaycastSystem::raycast(
@@ -306,11 +415,14 @@ impl World {
         );
         let end_time = get_time();
         let elapsed_time = end_time - start_time;
-        draw_text(&format!("Raycasting FPS: {}", 1.0/ elapsed_time),  10.0, 30.0, 20.0, RED);
+        RenderPlayerPOV::render_floor(self.player.angle, player_ray_origin, &self.floor_layout);
+        RenderPlayerPOV::render_world(&raycast_results);
+
         RenderMap::render_world_layout(&self.world_layout);
         RenderMap::render_player_on_map(self.player.pos);
         RenderMap::render_rays(player_ray_origin, &raycast_results);
-        RenderPlayerPOV::render(&raycast_results);
+
+        draw_text(&format!("Raycasting FPS: {}", 1.0 / elapsed_time), 10.0, 30.0, 20.0, RED);
     }
 }
 #[macroquad::main(window_conf)]
